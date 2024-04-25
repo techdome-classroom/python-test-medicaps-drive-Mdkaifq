05904ca1d3beae798864a546e69fa376a5265cbf
Mdkaifq <kaifqureshi360@gmail.com>
2024-04-25 08:31:00 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -5,12 +5,12 @@ class Solution(object):
         :rtype: bool
         """
         stack = []
-    mapping = {')': '(', '}': '{', ']': '['}
+        mapping = {')': '(', '}': '{', ']': '['}
 
-    for char in s:
-        if char in mapping.values():
-            stack.append(char)
-        elif char in mapping.keys():
+        for char in s:
+            if char in mapping.values():
+                stack.append(char)
+            elif char in mapping.keys():
             if stack == [] or mapping[char] != stack.pop():
                 return False
         else:
