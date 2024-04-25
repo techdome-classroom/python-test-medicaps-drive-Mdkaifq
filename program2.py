76321a5bc023a59af65a78aa571fd35503fadc50
Mdkaifq <kaifqureshi360@gmail.com>
2024-04-25 08:16:34 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -4,29 +4,7 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        roman_numerals = {
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-        'M': 1000
-             }
         
-
-         result = 0
-         prev_value = 0
-
-         for numeral in reversed(roman):
-        value = roman_numerals[numeral]
-        if value < prev_value:
-            result -= value
-        else:
-            result += value
-        prev_value = value
-
-    return result
 
 
 
