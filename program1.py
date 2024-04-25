72ee32fa50b95830e044bc994bce884b44bd2c3b
Mdkaifq <kaifqureshi360@gmail.com>
2024-04-25 08:25:55 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -6,9 +6,9 @@ class Solution(object):
         """
         count = 0
     
-    for char in s:
-        if char == '(':
-            count += 1
+        for char in s:
+            if char == '(':
+                count += 1
         elif char == ')':
             count -= 1
             if count < 0:
